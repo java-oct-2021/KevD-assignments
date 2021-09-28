f0933c80fe3d2138c3285ef3396970e89ee74682
kevd82 <80932709+kevd82@users.noreply.github.com>
2021-09-27 22:25:26 -0700

    Add files via upload

---
+++ 01-java-fundamentals/01-Java First Program/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("My name is Kevin Deming");
+        System.out.println("I am 53 years old");
+        System.out.println("My hometown is Fort Wayne, Indiana");
+    }
+}
